5ff66f83c6132669dfabb7bd0ed2aebacce434cb
igorj <igor.castro_ext@light.com.br>
2025-11-01 06:23:34 -0300

    first commit

---
+++ src/main/java/com/fiap/gerencia_encomenda/GerenciaEncomendaApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.gerencia_encomenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciaEncomendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciaEncomendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/gerencia_encomenda/GerenciaEncomendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.gerencia_encomenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciaEncomendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
